4e7185a7a5d62c6b666f8bb0e021181ff14aa467
Suyash Mohan <suyashatweb@gmail.com>
2020-09-29 18:46:30 +0800

    add linux support

---
+++ src/libs.c
@@ -0,0 +1,9 @@
+#ifndef __APPLE__
+
+#define SOKOL_IMPL
+#define SOKOL_GLCORE33
+#include "../libs/sokol/sokol_app.h"
+#include "../libs/sokol/sokol_gfx.h"
+#include "../libs/sokol/sokol_glue.h"
+
+#endif
